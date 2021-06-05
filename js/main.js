9e0a421248db0def88c95d35ccb3d1762060af79
Lam Pham <completejavascript.super@gmail.com>
2021-06-05 16:41:08 +0700

    Remove cors-anywhere because of unavailable & replace darksky api to own api

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ $(document).ready(() => {
   let $next48hSummary = $next48hContent.find(".summary");
   let $next48hHumidity = $next48hContent.find(".humidity .detail");
   let $next48hSelect = $(".next-48h .heading .select-style select");
-  let dataNext48h, indexNext48h = 1, next48hSelectInit = false;
+  let dataNext48h,
+    indexNext48h = 1,
+    next48hSelectInit = false;
 
   let $next7dContent = $(".next-7d .content");
   let $day = $next7dContent.find(".day");
@@ -19,53 +21,56 @@ $(document).ready(() => {
   let $next7dSummary = $next7dContent.find(".summary");
   let $next7dHumidity = $next7dContent.find(".humidity .detail");
   let $next7dSelect = $(".next-7d .heading .select-style select");
-  let dataNext7d, indexNext7d = 1, next7dSelectInit = false;
-  
-  let skycons = new Skycons({"color": "#3da4ab"});
-  let F2C = F => ((F - 32) * 5 / 9).toFixed();
-  let C2F = C => (C * 9 / 5 + 32).toFixed();
+  let dataNext7d,
+    indexNext7d = 1,
+    next7dSelectInit = false;
+
+  let skycons = new Skycons({ color: "#3da4ab" });
+  let F2C = (F) => (((F - 32) * 5) / 9).toFixed();
+  let C2F = (C) => ((C * 9) / 5 + 32).toFixed();
   let currentUnit = "F";
 
-  $(".icon").on("click", event => {
+  $(".icon").on("click", (event) => {
     let $target = $(event.target);
     $target.closest(".heading").find(".menu").toggleClass("responsive");
   });
 
-  $(".menu .item").on("click", event => {
+  $(".menu .item").on("click", (event) => {
     let $target = $(event.target);
     let unit = $target.attr("unit");
     $target.closest(".menu").children().removeClass("active");
     $target.addClass("active");
-    
+
     if (unit !== currentUnit) {
       currentUnit = unit;
       switchTemperatureUnit();
     }
   });
 
-  // Automatically enable cross-domain requests when needed
-  $.ajaxPrefilter(options => {
-    if (options.crossDomain && $.support.cors) {
-      options.url = `https://cors-anywhere.herokuapp.com/${options.url}`;
-    }
-  });
-
-  $.getJSON("https://ipinfo.io/", onLocationGot);
+  $.getJSON("https://ipinfo.io/json?token=527fdee7d62268", onLocationGot);
 
   function onLocationGot(info) {
     $location.text(`${info.city}, ${info.region}`);
     updateWeatherInfo.apply(null, info.loc.split(","));
   }
 
-  function updateWeatherInfo(lat, lon) {
-    let secretKey = "9b0af02f1ad5f739d0fd7a754e459238";
-    let requestURL = `https://api.darksky.net/forecast/${secretKey}/${lat},${lon}`;
+  function updateWeatherInfo(lat, lng) {
+    let requestURL = `https://simple-darksky-server.glitch.me/?lat=${lat}&lng=${lng}`;
 
-    $.getJSON(requestURL, info => {
-      highlightView();
-      updateWeatherCurrently(info.currently, $currentTemp, $currentSummary, $currentHumidity, "current-icon");
-      updateWeatherNext48h(info.hourly.data, indexNext48h);
-      updateWeatherNext7d(info.daily.data, indexNext7d);
+    $.getJSON(requestURL, (info) => {
+      let { code, data } = info;
+      if (code === 200) {
+        highlightView();
+        updateWeatherCurrently(
+          data.currently,
+          $currentTemp,
+          $currentSummary,
+          $currentHumidity,
+          "current-icon"
+        );
+        updateWeatherNext48h(data.hourly.data, indexNext48h);
+        updateWeatherNext7d(data.daily.data, indexNext7d);
+      }
     });
   }
 
@@ -74,11 +79,17 @@ $(document).ready(() => {
     $(".main-app").css("opacity", 1);
   }
 
-  function updateWeatherCurrently(infoCurrently, $temp, $summary, $humidity, idIcon) {
+  function updateWeatherCurrently(
+    infoCurrently,
+    $temp,
+    $summary,
+    $humidity,
+    idIcon
+  ) {
     let curTemp = infoCurrently.temperature.toFixed();
-	if (currentUnit === "C") curTemp = F2C(curTemp);
-	
-    $temp.text(`${curTemp}°${currentUnit}`); 
+    if (currentUnit === "C") curTemp = F2C(curTemp);
+
+    $temp.text(`${curTemp}°${currentUnit}`);
     $temp.attr("value", curTemp);
 
     $summary.text(infoCurrently.summary);
@@ -90,19 +101,39 @@ $(document).ready(() => {
     dataNext48h = infoHourlyData;
     let dataChoosen = dataNext48h[index];
     let date = new Date(dataChoosen.time * 1000);
-    let formatDate = date.toLocaleString("en-US", {weekday: 'long', hour: '2-digit', minute:'2-digit'});
+    let formatDate = date.toLocaleString("en-US", {
+      weekday: "long",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
     $time.text(formatDate);
 
-    updateWeatherCurrently(dataChoosen, $next48hTemp, $next48hSummary, $next48hHumidity, "next48h-icon");
+    updateWeatherCurrently(
+      dataChoosen,
+      $next48hTemp,
+      $next48hSummary,
+      $next48hHumidity,
+      "next48h-icon"
+    );
 
-    if(next48hSelectInit == false) {
+    if (next48hSelectInit == false) {
       let dataSelect = [];
-      for(let i = 1; i < dataNext48h.length; i += 6) {
+      for (let i = 1; i < dataNext48h.length; i += 6) {
         date = new Date(dataNext48h[i].time * 1000);
-        formatDate = date.toLocaleString("en-US", {weekday: 'long', hour: '2-digit', minute:'2-digit'});
+        formatDate = date.toLocaleString("en-US", {
+          weekday: "long",
+          hour: "2-digit",
+          minute: "2-digit",
+        });
         dataSelect.push(formatDate);
       }
-      initSelect($next48hSelect, dataSelect, updateWeatherNext48h, 6, dataNext48h);
+      initSelect(
+        $next48hSelect,
+        dataSelect,
+        updateWeatherNext48h,
+        6,
+        dataNext48h
+      );
       next48hSelectInit = true;
     }
   }
@@ -111,17 +142,17 @@ $(document).ready(() => {
     dataNext7d = infoDailyData;
     let dataChoosen = dataNext7d[index];
     let date = new Date(dataChoosen.time * 1000);
-    let formatDate = date.toLocaleString("en-US", {weekday: 'long'});
+    let formatDate = date.toLocaleString("en-US", { weekday: "long" });
     $day.text(formatDate);
 
     let minTemp = dataChoosen.temperatureMin.toFixed();
     let maxTemp = dataChoosen.temperatureMax.toFixed();
-	
-	if (currentUnit === "C") {
-	  minTemp = F2C(minTemp);
-	  maxTemp = F2C(maxTemp);
-	}
-	
+
+    if (currentUnit === "C") {
+      minTemp = F2C(minTemp);
+      maxTemp = F2C(maxTemp);
+    }
+
     $next7dTemp.text(`${minTemp}°${currentUnit} / ${maxTemp}°${currentUnit}`);
     $next7dTemp.attr("min", minTemp);
     $next7dTemp.attr("max", maxTemp);
@@ -130,11 +161,11 @@ $(document).ready(() => {
     $next7dHumidity.text(`${(dataChoosen.humidity * 100).toFixed()}%`);
     setWeatherIcon("next7d-icon", dataChoosen.icon);
 
-    if(next7dSelectInit == false) {
+    if (next7dSelectInit == false) {
       let dataSelect = [];
-      for(let i = 1; i < dataNext7d.length; i++) {
+      for (let i = 1; i < dataNext7d.length; i++) {
         date = new Date(dataNext7d[i].time * 1000);
-        formatDate = date.toLocaleString("en-US", {weekday: 'long'});
+        formatDate = date.toLocaleString("en-US", { weekday: "long" });
         dataSelect.push(formatDate);
       }
       initSelect($next7dSelect, dataSelect, updateWeatherNext7d, 1, dataNext7d);
@@ -143,7 +174,7 @@ $(document).ready(() => {
   }
 
   function initSelect($select, dataSelect, updateFunc, step, data) {
-    dataSelect.forEach(element => {
+    dataSelect.forEach((element) => {
       $select.append(`<option>${element}</option>`);
     });
     $select.on("change", (event) => {
@@ -183,30 +214,31 @@ $(document).ready(() => {
   function switchTemperatureUnit() {
     switchTemperatureUnitByItem($currentTemp);
     switchTemperatureUnitByItem($next48hTemp);
-	switchTemperatureUnitNext7d();
+    switchTemperatureUnitNext7d();
   }
 
   function switchTemperatureUnitByItem($item) {
-    let currentTemp = $item.attr("value"), newTemp;
+    let currentTemp = $item.attr("value"),
+      newTemp;
 
-    if(currentUnit === "F") newTemp = C2F(currentTemp);
+    if (currentUnit === "F") newTemp = C2F(currentTemp);
     else newTemp = F2C(currentTemp);
 
     $item.text(`${newTemp}°${currentUnit}`);
     $item.attr("value", newTemp);
   }
-  
+
   function switchTemperatureUnitNext7d() {
-	let minTemp = $next7dTemp.attr("min"), maxTemp = $next7dTemp.attr("max");
+    let minTemp = $next7dTemp.attr("min"),
+      maxTemp = $next7dTemp.attr("max");
     let newMin, newMax;
 
-    if(currentUnit === "F") {
+    if (currentUnit === "F") {
       newMin = C2F(minTemp);
       newMax = C2F(maxTemp);
-    }
-    else {
+    } else {
       newMin = F2C(minTemp);
-      newMax = F2C(maxTemp); 
+      newMax = F2C(maxTemp);
     }
     $next7dTemp.text(`${newMin}°${currentUnit} / ${newMax}°${currentUnit}`);
     $next7dTemp.attr("min", newMin);
